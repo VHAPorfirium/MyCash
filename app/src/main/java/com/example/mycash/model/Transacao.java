6f410824233649f287192a40a8efe6a0d8bde673
Victor Hugo <vhaporfiro@gmail.com>
2025-03-25 09:37:58 -0300

    Criação das activitys e criação da classe transação.

---
+++ app/src/main/java/com/example/mycash/model/Transacao.java
@@ -0,0 +1,60 @@
+package com.example.mycash.model;
+
+import java.util.Date;
+
+public class Transacao {
+    private int id;
+    private String descricao;
+    private double valor;
+    private String tipo;
+    private Date data;
+
+    public Transacao(int id, String descricao, double valor, String tipo, Date data) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.data = data;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
+
